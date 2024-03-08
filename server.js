785e551084795368f52eeb598a7ecfb3317060fe
JohnChewning <johnathanchewning@gmail.com>
2024-03-07 21:14:10 -0500

    added a temporary get for main.handlebars

--- server.js
+++ server.js
@@ -35,6 +35,11 @@ app.use(session(sess));
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+// Define a route to render the main template
+app.get('/', (req, res) => {
+  res.render('layouts/main', { pageTitle: 'SnouteRoute' });
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
