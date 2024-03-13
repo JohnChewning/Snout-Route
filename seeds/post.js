2b23558899319898ea885e1ee3db9aed67af1e51
RRHunterH <102266063+RRHunterH@users.noreply.github.com>
2024-03-12 21:00:41 -0400

    created seeds
    updated user.js, post.js, & comment.js to seed user, post & comment data

---
+++ seeds/post.js
@@ -0,0 +1,12 @@
+const db = require('./config/db');
+
+const users = [
+  { username: 'user-1', email: 'user-1@example.com' },
+  { username: 'user-2', email: 'user-2@example.com' },
+];
+
+users.forEach(user => {
+  db.addUser(user);
+});
+
+console.log('Users seeded:', db.users);
