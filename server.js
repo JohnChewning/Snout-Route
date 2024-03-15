04d5677ddb13c567724e8fa0401ac768ced3bdf8
tmaraki <toshiemaraki@gmail.com>
2024-03-14 21:14:54 -0400

    updated background image and cleaned up handlebars

--- server.js
+++ server.js
@@ -45,6 +45,7 @@ app.get('/', (req, res) => {
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('images'));
 
 app.use(routes);
 
