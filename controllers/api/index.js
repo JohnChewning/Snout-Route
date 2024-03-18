aabe5e434d1ed4e6c7f602668a84c86a3a8cde7e
JohnChewning <johnathanchewning@gmail.com>
2024-03-18 15:02:20 -0400

    fixed user routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -6,6 +6,10 @@ const locationRoutes = require('./locationRoutes');
 // const reviewRoutes = require('./reviewRoutes');
 const userRoutes = require('./userRoutes');
 
+router.get('/', (req, res) => {
+    res.json({ message: 'API endpoint is working!' });
+  });
+  
 // Mount locationRoutes and reviewRoutes
 router.use('/locations', locationRoutes);
 // router.use('/reviews', reviewRoutes);
--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -3,44 +3,41 @@ const { User, Review, Location } = require('../../models');
 
 // Get all users
 router.get('/', (req, res) => {
-    User.findAll({
-        attributes: {
-            exclude: ['password']
-        },
-        where: {
-            id: req.params.id
-        },
-    })
-        .then(dbUserData => res.json(dbUserData))
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
-});
-
-// Get specific user
-router.get('/:id', (req, res) => {
-    User.findOne({
-        attributes: {
-            exclude: ['password']
-        },
-        where: {
-            id: req.params.id
-        },
-    })
-        .then(dbUserData => {
-            if (!dbUserData) {
-                res.status(404).json({
-                    message: 'No user found with this id'
-                });
-                return;
-            }
-            res.json(dbUserData);
+    const userId = req.query.id;
+    if (userId) {
+        User.findOne({
+            attributes: {
+                exclude: ['password']
+            },
+            where: {
+                id: userId
+            },
         })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({
+                        message: 'No user found with this id'
+                    });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(500).json(err);
+            });
+    } else {
+        User.findAll({
+            attributes: {
+                exclude: ['password']
+            }
+        })
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => {
+                console.log(err);
+                res.status(500).json(err);
+            });
+    }
 });
 
 // Create a user
